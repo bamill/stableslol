9e2c49438dd2b8673155b139c8d420e9b4b9b05b
Bret MILLER <abunai@sashizu.me>
2017-03-10 22:45:34 -0800

    normed and added static safe_free function

--- stables.c
+++ stables.c
@@ -1,51 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stables.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmiller <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/10 22:29:55 by bmiller           #+#    #+#             */
+/*   Updated: 2017/03/10 22:38:44 by bmiller          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-long	sum_array(int *array, int size) {
-  long	sum;
-  int	index;
-
-  sum = 0;
-  index = 0;
-  while(index < size) {
-    sum += array[index];
-    index++;
-  }
-  return(sum);
+static void		safe_free(void **pp)
+{
+	if (pp != NULL)
+	{
+		free(*pp);
+		*pp = NULL;
+	}
 }
 
-void	allocate_loads_of_memory(void) {
-  int	*int_pointer_powa;
-  char	*pointer_powa;
-  int	*another_pointer;
-  int	index;
+long			sum_array(int *array, int size)
+{
+	long		sum;
+	int			index;
 
-  index = 0;
-  int_pointer_powa = (int*)malloc(sizeof(int) * 4242);
-  pointer_powa = (char*)malloc(sizeof(char*) * 42);
-  another_pointer = (int*)malloc(sizeof(int) * 424242);
-
-  memset(pointer_powa, 42, 42);
-  memset(int_pointer_powa, 42, 4242);
-  while(index < 424242) {
-    another_pointer[index] = index * 42;
-    index++;
-  }
-
-  printf("%ld\n", sum_array(int_pointer_powa, 4242));
-  printf("%s\n", pointer_powa);
-  printf("%ld\n", sum_array(another_pointer, 424242));
-  free(int_pointer_powa);
-  int_pointer_powa = NULL;
-  free(pointer_powa);
-  pointer_powa = NULL;
-  free(another_pointer);
-  another_pointer = NULL;
+	sum = 0;
+	index = 0;
+	while (index < size)
+	{
+		sum += array[index];
+		index++;
+	}
+	return (sum);
 }
 
-int main(void) {
-  allocate_loads_of_memory();
+void			allocate_loads_of_memory(void)
+{
+	int			*int_pointer_powa;
+	char		*pointer_powa;
+	int			*another_pointer;
+	int			index;
 
-  return(0);
+	index = 0;
+	int_pointer_powa = (int*)malloc(sizeof(int) * 4242);
+	pointer_powa = (char*)malloc(sizeof(char*) * 42);
+	another_pointer = (int*)malloc(sizeof(int) * 424242);
+	memset(pointer_powa, 42, 42);
+	memset(int_pointer_powa, 42, 4242);
+	while (index < 424242)
+	{
+		another_pointer[index] = index * 42;
+		index++;
+	}
+	printf("%ld\n", sum_array(int_pointer_powa, 4242));
+	printf("%s\n", pointer_powa);
+	printf("%ld\n", sum_array(another_pointer, 424242));
+	safe_free((void**)&int_pointer_powa);
+	safe_free((void**)&pointer_powa);
+	safe_free((void**)&another_pointer);
+}
+
+int				main(void)
+{
+	allocate_loads_of_memory();
+	return (0);
 }
